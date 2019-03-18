c65c331c9052de34c2cfea6234a69e2303e67c3c
yann.doudin <yann.doudin@gmail.com>
2019-03-18 16:50:58 +0100

    Resume architecture done

---
+++ react-client/src/components/pages/studiesComponent/studies.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+class Studies extends Component {
+
+  render() {
+    return (
+      <div>
+        Lorem ipsum dolor sit amet, c
+      </div>
+    )
+  }
+}
+
+export default Studies;
